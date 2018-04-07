4142e31f91224dba75e047fe663d0802085e60a5
moranpatrick <patrickmoran121@gmail.com>
2018-04-07 03:45:29 +0100

    Users can now download a text file from a url for Running through the SA Algorithm

--- ArtificialIntelligenceProject/src/ie/gmit/sw/ai/CipherBreaker.java
+++ ArtificialIntelligenceProject/src/ie/gmit/sw/ai/CipherBreaker.java
@@ -5,6 +5,13 @@ package ie.gmit.sw.ai;
  */
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -19,16 +26,18 @@ public class CipherBreaker {
 		String cipherText = "";
 		SimulatedAnnealing sa = null;
 		String key;
-		String result;
+		String result, urlFile, fileLocation;
 		File file;
 		String output;		
-		PlayfairCipher pf = new PlayfairCipher();		
+		PlayfairCipher pf = new PlayfairCipher();	
+		Path pathFileUrl = null;
 		
 		do{
 			try {
 				choice = menu();
 				switch(choice){
 				case 1:
+					/*Decrypt Cipher Text From Users Key */
 					System.out.println("Please Enter Your Key:");
 					key = sc.next();
 					
@@ -56,6 +65,7 @@ public class CipherBreaker {
 					}
 					break;
 				case 2:
+					/*Decrypt Cipher Text Using SA */
 					System.out.println("Load 4Grams file? (y/n)");
 					load = sc.next();
 					
@@ -90,9 +100,66 @@ public class CipherBreaker {
 					} catch (IOException e) {
 						System.out.println("Unable to load %s" + fileName);
 					}						
-					break;		
+					break;	
+				case 3:
+					/*Decrypt Cipher Text From Text File from URL */
+					System.out.println("Enter URL to Text File (eg www.google.com/file.txt)");
+					fileLocation = sc.next();
+					
+					try {
+						URL url = new URL(fileLocation);
+						InputStream in = url.openStream();
+						
+						urlFile = fileLocation.substring(fileLocation.lastIndexOf('/') + 1, fileLocation.length());
+						pathFileUrl = Paths.get("./" + urlFile).toAbsolutePath().normalize();
+						System.out.println("Downloading to " + pathFileUrl.toString());
+						System.out.println(url.getFile().toString());
+					    Files.copy(in, pathFileUrl , StandardCopyOption.REPLACE_EXISTING);
+					    System.out.println("Download complete...");
+					} catch (MalformedURLException mue) {
+						System.out.println("Invalid Url - Please Check Again");
+						continue;
+					} catch (Exception e) {
+						System.out.println("Error getting book.." + e);
+						continue;
+					}
+					System.out.println("Text File Downloaded. Processing ..." + pathFileUrl.toString());
+					
+					System.out.println("Load 4Grams file? (y/n)");
+					load = sc.next();
+					
+					if(load.equalsIgnoreCase("y") || load.equalsIgnoreCase("yes")){
+						System.out.println("Please enter path to 4grams file (eg C:\\4grams.txt): ");
+						gramsPath = sc.next();											
+						GramParser gp = new GramParser();
+						try {
+							gp.parse4Gram(gramsPath);
+						} catch (FileNotFoundException e){
+							System.out.println("Unable To Locate nGrams File - Please Check Path");
+							continue;
+						} catch (IOException e) {
+							System.out.println("Unable to load the nGrams.txt");
+							continue;
+						}
+					}
+					else{
+						System.out.println("Sorry - That was invalid input.\n");
+						continue;
+					}
+					
+					try {
+						cipherText = new File().readFile(pathFileUrl.toString()).toUpperCase().replaceAll("[^A-Za-z0-9 ]", "");
+						sa = new SimulatedAnnealing();
+						sa.StartSimulatedAnnealing(cipherText);
+							
+					} catch (FileNotFoundException e) {
+						System.out.println("Unable To Locate The File - Please Check Path");
+					} catch (IOException e) {
+						System.out.println("Unable to load %s" + pathFileUrl.toString());
+					}					
+					break;
 				default:
-					if(choice != 3){
+					if(choice != 4){
 						System.out.println("Sorry - Thats invalid Input Please Try Again");
 						break;
 					}
@@ -101,7 +168,7 @@ public class CipherBreaker {
 				System.out.println("Sorry - Thats invalid Input Please Try Again");
 				sc.next();
 			}			
-		}while(choice != 3);				
+		}while(choice != 4);				
 		sc.close();
 	}
     
@@ -111,13 +178,14 @@ public class CipherBreaker {
 	 */
     public static int menu() {
         int choice;   
-        System.out.println("|-------------------------|");
-        System.out.println("|------- Main Menu -------|");
-        System.out.println("|-------------------------|");
-        System.out.println("|  1) Decrypt With Key    |");
-        System.out.println("|  2) Decrypt Without Key |");       
-        System.out.println("|  3) Quit                |");     
-        System.out.println("|-------------------------|");
+        System.out.println("|---------------------------|");
+        System.out.println("|-------- Main Menu --------|");
+        System.out.println("|---------------------------|");
+        System.out.println("|  1) Decrypt With Key      |");
+        System.out.println("|  2) Decrypt Without Key   |");       
+        System.out.println("|  3) Decrypt text from URL |");     
+        System.out.println("|  4) Quit                  |");     
+        System.out.println("|---------------------------|");
 
         choice = sc.nextInt();
         return choice;    
